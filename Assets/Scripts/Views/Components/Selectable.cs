adcb1c881b41f4a2be0c8152c755a63bb666a5b6
Alexander Lövberg <lovbergalexander@gmail.com>
2017-10-24 08:08:39 +0200

    Selectable script

---
+++ Assets/Scripts/Views/Components/Selectable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selectable : MonoBehaviour {
+
+	public void Select(){
+		Debug.Log(transform.name + " selected");
+	}
+
+	public void DeSelect(){
+		Debug.Log(transform.name + " deselected");		
+	}
+}
